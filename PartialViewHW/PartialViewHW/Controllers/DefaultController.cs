c20cd786bdba8bcb4cb416fae75741a90e4456d6
ykizilirmak <ykizilirmak93@gmail.com>
2020-12-31 21:13:29 +0300

    HomeWork

---
+++ PartialViewHW/PartialViewHW/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PartialViewHW.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+         public IActionResult Detay(Models.urun data)
+        {
+            var veri=new Models.urun();
+            veri.adi = "bot";
+            veri.fiyati = 150;
+            veri.kullanilmamislikdurumu = true;
+            return View(veri);
+        }
+    }
+}
---
+++ PartialViewHW/PartialViewHW/Models/urun.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartialViewHW.Models
+{
+    public class urun
+    {
+        public string adi { get; set; }
+        public int fiyati { get; set; }
+        public bool kullanilmamislikdurumu { get; set; }
+        
+    }
+}
