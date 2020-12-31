e51bd22fa0b33255c5968852938a963c350bf6a3
ykizilirmak <ykizilirmak93@gmail.com>
2020-12-31 23:42:41 +0300

    İKİNCİ PROJE

---
+++ PartialViewHomework/PartialViewHomework/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PartialViewHomework.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult _LoginPartial()
+        {
+            return PartialView();
+        }
+        public IActionResult _sign()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ PartialViewHomework/PartialViewHomework/Models/username.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartialViewHomework.Models
+{
+    public class username
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
