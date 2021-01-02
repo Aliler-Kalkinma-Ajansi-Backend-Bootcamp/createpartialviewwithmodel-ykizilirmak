e2727d265f6b9aaf6f78c5afef56cb7b3b0492df
ykizilirmak <ykizilirmak93@gmail.com>
2021-01-02 23:17:57 +0300

    son update

---
+++ PartialView/PartialView/Controllers/DefaultController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PartialView.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Parcali()
+        {
+            return View();
+        }
+        public ActionResult _Layout()
+        {
+            return View();
+        }
+        public ActionResult _Menu()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ Project2/Project2/Kaynak.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "merhaba C++\n";
+	system("PAUSE");
+	return 0;
+}
